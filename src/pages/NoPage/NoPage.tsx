4eb17b4e651c8c7fd4819dda33f6cd9e9e4da6f2
ntnshpig <dableboble@gmail.com>
2022-05-02 13:35:40 +0300

    change img tag ro Box

--- src/pages/NoPage/NoPage.tsx
+++ src/pages/NoPage/NoPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BoxInnerStyled, MainBoxStyled } from './NoPageStyles'
 import { useLocation } from 'react-router-dom'
 import Gif404 from '../../assets/imgs/404.gif'
-import { Link, Typography } from '@mui/material'
+import { Box, Link, Typography } from '@mui/material'
 
 const NoPage = () => {
   const location = useLocation()
@@ -10,9 +10,13 @@ const NoPage = () => {
   return (
     <MainBoxStyled>
       <BoxInnerStyled>
-        <img style={{ width: 'clamp(200px, 70%, 700px)' }} src={Gif404} />
-        <Typography variant="h4" mt={1}>Page {location.pathname} was not found! </Typography>
-        <Link mt={1} href="/">Back to home</Link>
+        <Box component={'img'} style={{ width: 'clamp(200px, 70%, 700px)' }} src={Gif404} />
+        <Typography variant="h4" mt={1}>
+          Page {location.pathname} was not found!{' '}
+        </Typography>
+        <Link mt={1} href="/">
+          Back to home
+        </Link>
       </BoxInnerStyled>
     </MainBoxStyled>
   )
